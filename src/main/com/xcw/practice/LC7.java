9d13b9327e60f71b74afabdef480a15b2af18a37
wangxuechao <1263629116@qq.com>
2021-07-06 16:56:18 +0800

    LC7 PUT

--- src/main/com/xcw/practice/LC7.java
+++ src/main/com/xcw/practice/LC7.java
@@ -7,41 +7,11 @@ package com.xcw.practice;
 public class LC7 {
 
     public static void main(String[] args) {
-        System.out.println(reverse1(120));
+        System.out.println(reverse(120));
     }
+
 
     public static int reverse(int x) {
-        if(x == 0) return x;
-        String s = String.valueOf(x);
-        StringBuffer sb = new StringBuffer();
-        String pre = "";
-        if(x < 0){
-            s = s.substring(1);
-            sb.append(s);
-            pre = "-";
-        }else{
-            sb.append(s);
-        }
-
-        s = sb.reverse().toString();
-        if(s.startsWith("0")) s = s.substring(1);
-        s = pre + s;
-
-        String s1 = Integer.MAX_VALUE + "";
-        String s2 = Integer.MIN_VALUE + "";
-        if(s.length() < s1.length()) return Integer.valueOf(s).intValue();
-        if(s.startsWith("-") && s.charAt(1) >= s2.charAt(1) && Integer.valueOf(s.substring(2)).intValue() > Integer.valueOf(s2.substring(2))) {
-            return 0;
-        }
-
-        if(!s.startsWith("-") && s.charAt(0) >= s2.charAt(0) && Integer.valueOf(s.substring(1)).intValue() > Integer.valueOf(s1.substring(1))) {
-            return 0;
-        }
-        return Integer.valueOf(s).intValue();
-
-    }
-
-    public static int reverse1(int x) {
         int result = 0;
         while (x != 0) {
             if(result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10) {
