89b8837dfbcf810b199548733d11039302bec902
wangxuechao <1263629116@qq.com>
2021-07-07 14:59:18 +0800

    LC26 删除有序数组中重复元素

---
+++ src/main/com/xcw/practice/LC26.java
@@ -0,0 +1,35 @@
+package com.xcw.practice;
+/**
+ * @author wangxuechao
+ * @date 2021-07-07
+ */
+public class LC26 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {0};
+        removeDuplicates(nums);
+    }
+
+    /**
+     * 双指针法删除有序数组中重复元素
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates(int[] nums) {
+
+        if(nums.length == 0) {
+            return nums.length;
+        }
+        int front = 0,next = 1;
+        while (next < nums.length) {
+            if(nums[front] == nums[next]) {
+                next++;
+            }else {
+                front++;
+                nums[front] = nums[next];
+            }
+        }
+        return front + 1;
+    }
+}
